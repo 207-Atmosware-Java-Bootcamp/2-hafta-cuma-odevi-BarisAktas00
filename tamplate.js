e5e227cd821aac13100534b814bb2502e64b1ae6
Barış Aktaş <h.b.aktas5293@gmail.com>
2022-06-04 00:25:48 +0300

    revize
    revize

--- tamplate.js
+++ tamplate.js
@@ -5,21 +5,18 @@ password2=prompt("Lütfen şifrenizi tekrar giriniz");
 
 function passwordCheck() {
 
-    while (password1!==password2 ) {
-        
-        alert("Şifreler uyuşmuyor.Tekrar deneyin.")
+while(password1!==password2){
+alert( "Şifreler eşleşmiyor")
+
+password1=prompt("Lütfen şifrenizi giriniz");
+password2=prompt("Lütfen şifrenizi tekrar giriniz");
+}
+    while(password1.length<6 || password2.length<6){
+alert("Şifre en az 6 haneli olmalıdır.")
 password1=prompt("Lütfen şifrenizi giriniz");
 password2=prompt("Lütfen şifrenizi tekrar giriniz");
     }
-   if((password1.length)<6 || (password2.length)){
-
-        alert("Şifre en az 6 haneli olmalıdır.")
-      password1=prompt("Lütfen şifrenizi giriniz");
-      password2=prompt("Lütfen şifrenizi tekrar giriniz");
-        }
-    return passwordCheck();
-    
+alert("Tebrikler")    
 }
-
-
 passwordCheck();
+
